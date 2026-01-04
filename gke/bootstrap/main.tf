2f82d69cd17272e64e452cd6650a65925bf76d1c
Serhiy Cherpatyuk <s.cherpatiuk@temabit.com>
2026-01-04 17:45:01 +0200

    Fix: Use Flux-managed patch for WIF annotation instead of Terraform

--- gke/bootstrap/main.tf
+++ gke/bootstrap/main.tf
@@ -105,18 +105,36 @@ resource "google_service_account_iam_member" "flux_wif_binding" {
   member             = "serviceAccount:${var.GOOGLE_PROJECT}.svc.id.goog[flux-system/kustomize-controller]"
 }
 
-# Flux Annotations
-resource "kubernetes_annotations" "flux_kms" {
-  api_version = "v1"
-  kind        = "ServiceAccount"
-  metadata {
-    name      = "kustomize-controller"
-    namespace = "flux-system"
-  }
-  annotations = {
-    "iam.gke.io/gcp-service-account" = google_service_account.flux_sa.email
-  }
-  depends_on = [module.flux_bootstrap]
+# Deploy Flux SA patch to enable Workload Identity
+resource "github_repository_file" "flux_sa_patch" {
+  repository = var.repository_name
+  branch     = "main"
+  file       = "flux-system/kustomize-controller-patch.yaml"
+  content = templatefile("${path.module}/../../templates/flux-sa-patch.yaml.tftpl", {
+    flux_sa_email = google_service_account.flux_sa.email
+  })
+  overwrite_on_create = true
+}
+
+# Kustomization to apply the patch
+resource "github_repository_file" "flux_kustomization" {
+  repository          = var.repository_name
+  branch              = "main"
+  file                = "flux-system/kustomization.yaml"
+  content             = <<-EOT
+    apiVersion: kustomize.config.k8s.io/v1beta1
+    kind: Kustomization
+    resources:
+      - gotk-components.yaml
+      - gotk-sync.yaml
+    patches:
+      - path: kustomize-controller-patch.yaml
+        target:
+          kind: ServiceAccount
+          name: kustomize-controller
+  EOT
+  overwrite_on_create = true
+  depends_on          = [module.flux_bootstrap]
 }
 
 output "workload_identity_provider" {
